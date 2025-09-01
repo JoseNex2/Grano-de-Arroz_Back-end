1ebc346c279bd01550ae14a4d8573d254e41b458
Valentin Martinez <valentin.martinezcmd@gmail.com>
2025-09-01 15:25:16 -0300

    Agregado de CORS

--- GDA/Program.cs
+++ GDA/Program.cs
@@ -18,12 +18,30 @@ namespace GDA
             UtilitiesInversionOfControl.AddDependency(services, configuration);
             DataAccessInversionOfControl.AddDependency(services, configuration);
 
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Desarrollo", policy =>
+                {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+                options.AddPolicy("Produccion", builder =>
+                {
+                    builder.WithOrigins(Environment.GetEnvironmentVariable("URL_DOMAIN"))
+                               .AllowAnyMethod()
+                               .AllowAnyHeader()
+                               .AllowCredentials();
+                });
+            });
+
             services.AddAuthorization();
 
             services.AddControllers();
 
             WebApplication app = builder.Build();
 
+            app.UseCors("Desarrollo");
             app.UseRouting();
             app.UseAuthentication();
             app.UseAuthorization();
