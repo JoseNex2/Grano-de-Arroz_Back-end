692bcaf345f2ce0b91cecb5952a5257481a1e006
Valentin Martinez <valentin.martinezcmd@gmail.com>
2025-11-01 23:33:12 -0300

    Agregando decoradores a la clase MetricsRecord

--- Entities/Domain/MetricsRecord.cs
+++ Entities/Domain/MetricsRecord.cs
@@ -1,8 +1,16 @@
-﻿namespace Entities.Domain
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Entities.Domain
 {
     public class MetricsRecord
     {
-        public string Id { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId _id { get; set; }
+        [BsonElement("Id")]
+        public int Id { get; set; }
+        [BsonElement("Metrics")]
         public Dictionary<string, float> Metrics { get; set; }
     }
 }
