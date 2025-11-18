a84c9b736a4b90887314431fae27feed17cc1ae2
Valentin Martinez <valentin.martinezcmd@gmail.com>
2025-11-18 19:16:12 -0300

    Agregado de relaciones para SecureRandomToken

--- Entities/Domain/SecureRandomToken.cs
+++ Entities/Domain/SecureRandomToken.cs
@@ -3,10 +3,11 @@
     public class SecureRandomToken
     {
         public int Id { get; set; }
-        public int UserId { get; set; }
         public string TokenHash { get; set; } = string.Empty;
         public DateTime ExpiredDate { get; set; }
         public bool Used { get; set; } = false;
         public DateTime CreatedDate { get; set; }
+        public int? UserId { get; set; }
+        public User? User { get; set; }
     }
 }
